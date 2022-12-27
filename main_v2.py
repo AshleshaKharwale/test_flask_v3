f437670f1a6137e753f7f854e45a884150641270
AshleshaKharwale <ashkharwale@gmail.com>
2022-12-27 21:58:20 +0530

    tasktwo api

--- main_v2.py
+++ main_v2.py
@@ -1,4 +1,6 @@
 import requests
+from typing import List
+from multiprocessing.pool import ThreadPool
 from flask import Flask, render_template
 from utils.randgen import ProduceChars
 from models.datamodels.characters import Characters
@@ -60,6 +62,62 @@ def task_one():
             primary_value = char
 
             insert_resource(table_name, primary_key, primary_value, columns, values)
+
+    return output
+
+
+@app.route("/tasktwo")
+def task_two():
+    """
+    This app fetches data of film1 and prints names of characters, vehicles, starships, species and
+    planets names.
+    """
+    film1_url = "https://swapi.dev/api/films/1"
+    response = requests.get(film1_url)
+    print(f"Fetching details from => {film1_url}\n")
+    response = response.json()
+    chars = response.get("characters")
+    planets = response.get("planets")
+    vehicles = response.get("vehicles")
+    starships = response.get("starships")
+    species = response.get("species")
+
+    def fetch_name(url:str) -> str:
+        """
+        Hits given url and converts to json data. Extracts only name value from it.
+        Returns name.
+        :param url: resource url
+        :return: name of resource
+        """
+        print(f"Fetching details from => {url}\n")
+        res = requests.get(url)
+        if res.status_code != 200:
+            return "Not found"
+        else:
+            res = res.json()
+        return res.get("name")
+
+    def get_names(resource:List[str])-> List[str]:
+        """
+        Returns name from each resource url for given list of urls.
+        :param resource: list or resource urls
+        :return: list of names
+        """
+        pool = ThreadPool(15)
+        names = pool.map(fetch_name, resource)
+        return names
+
+    char_names = get_names(chars)
+    planet_names = get_names(planets)
+    vehicle_names = get_names(vehicles)
+    starship_names = get_names(starships)
+    species_names = get_names(species)
+
+    output = {"characters": char_names,
+              "planets": planet_names,
+              "vehicles": vehicle_names,
+              "starships": starship_names,
+              "species": species_names}
 
     return output
 
